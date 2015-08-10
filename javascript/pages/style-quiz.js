a4d4042f259fd55e804357d764506cb031c34ead
Kaitlyn Noone <knoone@vipasolutions.com>
2015-08-10 13:20:49 -0500

    Additional changes to streamline

--- javascript/pages/style-quiz.js
+++ javascript/pages/style-quiz.js
@@ -16,6 +16,29 @@ jQuery(function($) {
     $forms.each(function(idx, form){
         var $form = $(form);
         var $questions = $form.find(".question");
+        var $colorQuestion = $(".question > .fav-color");
+        var $colorButtons = $colorQuestion.find(".button_description_container");
+        var $imageButtons = $questions.find('.button-image-container');
+
+        $colorButtons.each(function(idx, button) {
+            var $colorButton = $(button);
+
+            $colorButton.on('click', function(e){
+                $(this).siblings().removeClass(SELECTED_CLASS).addClass(UNSELECTED_CLASS).find("input[type=radio]").removeClass(FOCUSED_CLASS);
+                $(this).addClass(SELECTED_CLASS).removeClass(UNSELECTED_CLASS).find("input[type=radio]").addClass(FOCUSED_CLASS);
+            });
+        });
+
+        $imageButtons.each(function(idx, button) {
+            var $imageButton = $(button);
+
+            $imageButton.wrapInner('<div class="wrap" />');
+
+            $imageButton.on('click', function(e) {
+                $(this).parents('.button_description_container').addClass(SELECTED_CLASS).removeClass(UNSELECTED_CLASS).siblings().removeClass(SELECTED_CLASS).addClass(UNSELECTED_CLASS).find('.button-image-container').removeClass(CHECKED_CLASS);
+                $(this).addClass(CHECKED_CLASS).siblings(".rtb").find("input[type=radio]").attr(CHECKED_CLASS, true);
+            });
+        });
 
         if (!this.submit_options) {
             this.submit_options = {};
@@ -24,32 +47,5 @@ jQuery(function($) {
         this.submit_options.postUpdate = function() {
             initScrollPage(this);
         };
-
-        $questions.each(function(idx, question){
-            var $question = $(question);
-            var $imageButtons = $question.find('.button-image-container');
-            var $colorButtons = $question.find('.fav-color .button_description_container');
-
-
-            $imageButtons.each(function(idx, button) {
-                var $imageButton = $(button);
-
-                $imageButton.wrapInner('<div class="wrap" />')
-
-                $imageButton.on('click', function(e) {
-                    $(this).parents('.button_description_container').siblings().find('.button-image-container').removeClass(CHECKED_CLASS);
-                    $(this).addClass(CHECKED_CLASS).siblings(".rtb").find("input[type=radio]").attr(CHECKED_CLASS, true);
-                });
-            });
-
-            $colorButtons.each(function(idx, button) {
-               var $colorButton = $(button);
-
-                $colorButton.on('click', function(e){
-                    $(this).siblings().removeClass(SELECTED_CLASS).addClass(UNSELECTED_CLASS).find("input[type=radio]").removeClass(FOCUSED_CLASS);
-                    $(this).addClass(SELECTED_CLASS).removeClass(UNSELECTED_CLASS).find("input[type=radio]").addClass(FOCUSED_CLASS);
-                });
-            });
-        });
     });
 });
