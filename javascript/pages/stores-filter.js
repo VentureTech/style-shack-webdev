bd4bb6781a88bab6e88beccf09e82c422bc99489
Kaitlyn Noone <knoone@vipasolutions.com>
2015-08-13 17:01:57 -0500

    Adjusts click handler triggers

--- javascript/pages/stores-filter.js
+++ javascript/pages/stores-filter.js
@@ -1,14 +1,14 @@
 jQuery(function($){
-    $('.filters').each(function() {
+    $('.filters ul.filter_controls > .mi').each(function() {
         var $con = $(this);
         var $trigger = $con.find('.filter_label');
-        var $content = $con.find('.filter_controls');
-        var $form = $con.find('form');
+        var $content = $con.find('.t2_menu');
+        var $form = $con.parents('form');
 
         var CSS_OPENED_CLASS = 'opened';
 
         $trigger.on('click', function(evt) {
-            $con.toggleClass(CSS_OPENED_CLASS);
+            $content.toggleClass(CSS_OPENED_CLASS);
         });
 
         $content.on('change', 'input[type=checkbox]', function(evt) {
