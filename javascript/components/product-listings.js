6388cd0e137e66efb253bb872725da75b578ff1c
Kaitlyn Noone <knoone@vipasolutions.com>
2015-08-20 17:14:45 -0500

    Initial commit of files

---
+++ javascript/components/product-listings.js
@@ -0,0 +1,85 @@
+jQuery(function($){
+    var $storesCon = $(".products");
+    var $stores = $storesCon.find(".product");
+    var MOBILE_BREAKPOINT = 480;
+    var TABLET_BREAKPOINT = 1024;
+    var RESIZE_THROTTLE_TIME = 200;
+
+    var resizeThrottleId;
+
+    var $window = $(window);
+    var windowWidth = $window.width();
+
+    var COL_ONE_CLASS = "col1";
+    var COL_TWO_CLASS = "col2";
+    var COL_THREE_CLASS = "col3";
+    var COL_FOUR_CLASS = "col4";
+
+
+    function updateWindowWidth() {
+        windowWidth = $window.width();
+        orderStores();
+    }
+
+    function addResizeWatcher() {
+        $window.on('resize', function() {
+            clearTimeout(resizeThrottleId);
+            resizeThrottleId = setTimeout(function() {
+                updateWindowWidth();
+            }, RESIZE_THROTTLE_TIME);
+        });
+    }
+
+    function orderStores() {
+        $stores.each(function (idx, store) {
+            var $store = $(store);
+            $store.removeClass(COL_ONE_CLASS).removeClass(COL_TWO_CLASS).removeClass(COL_THREE_CLASS).removeClass(COL_FOUR_CLASS);
+
+            /** HANDLE FOUR COLS ABOVE TABLET LANDSCAPE **/
+            if (windowWidth > TABLET_BREAKPOINT) {
+                if (idx % 4 == 3) {
+                    $store.addClass(COL_FOUR_CLASS);
+                }
+                else if (idx % 4 == 2) {
+                    $store.addClass(COL_THREE_CLASS);
+                }
+                else if (idx % 4 == 1) {
+                    $store.addClass(COL_TWO_CLASS);
+                }
+                else {
+                    $store.addClass(COL_ONE_CLASS);
+                }
+            }
+
+            /** HANDLE THREE COLS BELOW TABLET LANDSCAPE AND ABOVE TABLET PORTRAIT **/
+            else if (windowWidth <= TABLET_BREAKPOINT && windowWidth > MOBILE_BREAKPOINT) {
+                if (idx % 3 == 2) {
+                    $store.addClass(COL_THREE_CLASS);
+                }
+                else if (idx % 3 == 1) {
+                    $store.addClass(COL_TWO_CLASS);
+                }
+                else {
+                    $store.addClass(COL_ONE_CLASS);
+                }
+            }
+
+            /** HANDLE TWO COLS BELOW TABLET PORTRAIT AND ABOVE MOBILE LANDSCAPE **/
+            else if (windowWidth <= TABLET_BREAKPOINT && windowWidth > MOBILE_BREAKPOINT) {
+                if (idx % 2 == 1) {
+                    $store.addClass(COL_TWO_CLASS);
+                }
+                else {
+                    $store.addClass(COL_ONE_CLASS);
+                }
+            }
+        });
+    }
+
+    function init() {
+        addResizeWatcher();
+        orderStores();
+    }
+
+    init();
+});
---
+++ javascript/pages/products-filter.js
@@ -0,0 +1,22 @@
+jQuery(function($){
+    $('.filters ul.filter_controls > .mi').each(function() {
+        var $con = $(this);
+        var $trigger = $con.find('.filter_label');
+        var $content = $con.find('.t2_menu');
+        var $form = $con.parents('form');
+
+        var CSS_OPENED_CLASS = 'opened';
+
+        $trigger.on('click', function(evt) {
+            $content.toggleClass(CSS_OPENED_CLASS);
+        });
+
+        $content.on('change', 'input[type=checkbox]', function(evt) {
+            $form.submit();
+        });
+
+        $content.on('change', 'input[type=radio]', function(evt) {
+            $form.submit();
+        });
+    });
+});
