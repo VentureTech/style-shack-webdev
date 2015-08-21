62063b13f03042fe5b45c3115a339453818f90de
Scott Benes <sbenes@vipasolutions.com>
2015-08-21 10:17:05 -0500

    Updates to build process and project organization

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,69 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var clip = require('gulp-clip-empty-files');
+var del = require('del');
 
-gulp.task('default', ['styles']);
+var SRC_PATH = './web/src';
+var BUILD_PATH = './web/build';
+var DESIGN_PATH = '/design';
+var JAVASCRIPT_PATH = '/javascript';
+var STYLESHEETS_PATH = '/stylesheets';
+var BUILD_ADMIN_PATH = BUILD_PATH + '/admin';
+var BUILD_FRONTEND_PATH = BUILD_PATH + '/frontend';
+var BUILD_STORES_PATH = BUILD_PATH + '/stores';
 
-gulp.task('styles', function () {
-	return gulp.src('./stylesheets/src/**/*.scss')
+//Design tasks
+gulp.task('design:clean', function(callback) {
+	del([
+		BUILD_ADMIN_PATH + DESIGN_PATH,
+		BUILD_FRONTEND_PATH + DESIGN_PATH,
+		BUILD_STORES_PATH + DESIGN_PATH
+	], callback);
+});
+
+gulp.task('design:build', ['design:clean'], function() {
+	return gulp
+		.src(SRC_PATH + DESIGN_PATH + '/**/*')
+		.pipe(gulp.dest(BUILD_ADMIN_PATH + DESIGN_PATH))
+		.pipe(gulp.dest(BUILD_FRONTEND_PATH + DESIGN_PATH))
+		.pipe(gulp.dest(BUILD_STORES_PATH + DESIGN_PATH));
+});
+
+gulp.task('design', ['design:build']);
+
+
+//Javascript tasks
+gulp.task('javascript:clean', function(callback) {
+	del([
+		BUILD_ADMIN_PATH + JAVASCRIPT_PATH,
+		BUILD_FRONTEND_PATH + JAVASCRIPT_PATH,
+		BUILD_STORES_PATH + JAVASCRIPT_PATH
+	], callback);
+});
+
+gulp.task('javascript:build', ['javascript:clean'], function() {
+	return gulp.src(SRC_PATH + JAVASCRIPT_PATH + '/**/*')
+		.pipe(gulp.dest(BUILD_ADMIN_PATH + JAVASCRIPT_PATH))
+		.pipe(gulp.dest(BUILD_FRONTEND_PATH + JAVASCRIPT_PATH))
+		.pipe(gulp.dest(BUILD_STORES_PATH + JAVASCRIPT_PATH));
+});
+
+gulp.task('javascript', ['javascript:build']);
+
+
+//Stylesheet tasks
+gulp.task('styles:clean', function(callback) {
+	del([
+		BUILD_ADMIN_PATH + STYLESHEETS_PATH,
+		BUILD_FRONTEND_PATH + STYLESHEETS_PATH,
+		BUILD_STORES_PATH + STYLESHEETS_PATH
+	], callback);
+});
+
+gulp.task('styles:build', ['styles:clean'], function () {
+	return gulp.src(SRC_PATH + STYLESHEETS_PATH + '/**/*.scss')
+		.pipe(clip())
 		.pipe(sass({
 			outputStyle: 'expanded'
 		}))
@@ -13,5 +71,12 @@ gulp.task('styles', function () {
 			browsers: ['> 1%', 'last 2 versions', 'ie >= 9'],
 			cascade: false
 		}))
-		.pipe(gulp.dest('./stylesheets/build'));
+		.pipe(gulp.dest(BUILD_ADMIN_PATH + STYLESHEETS_PATH))
+		.pipe(gulp.dest(BUILD_FRONTEND_PATH + STYLESHEETS_PATH))
+		.pipe(gulp.dest(BUILD_STORES_PATH + STYLESHEETS_PATH));
 });
+
+gulp.task('styles', ['styles:build']);
+
+gulp.task('default', ['app:build']);
+gulp.task('app:build', ['styles', 'design', 'javascript']);
