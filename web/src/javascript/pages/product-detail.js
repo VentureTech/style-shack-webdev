df272d4d26972ece60154c314c7fbf221b4938f2
Kaitlyn Noone <knoone@vipasolutions.com>
2015-08-25 17:19:30 -0500

    Attempts to handle picture switching. Think this is not quite there yet.

--- web/src/javascript/pages/product-detail.js
+++ web/src/javascript/pages/product-detail.js
@@ -1,31 +1,99 @@
 jQuery(function($){
-    var $infoCon = $(".info");
-    var $photoCon = $(".photos");
-    var OPEN_CLASS = "open";
+    var $infoCon = $(".more-info > div");
+    var $photoCon = $(".images");
+    var $forms = $("form");
+    var CSS_OPEN_CLASS = "open";
+    var CSS_EMPTY_CLASS = "empty";
+    var CSS_REMOVE_CLASS = "remove";
+    var CSS_OLD_CLASS = "old";
+    var CSS_NEW_CLASS = "new";
 
     function setupDescriptionToggle() {
         $infoCon.each(function (idx, con) {
             var $con = $(con);
-            var $proseDesc = $con.find(".prosedescription");
-            var $toggle = $proseDesc.find(".label");
-            var $content = $proseDesc.find(".content");
+            var $toggle = $con.find(".title");
+            var $content = $con.find(".content");
 
             $toggle.on("click", function (e) {
-                $toggle.toggleClass(OPEN_CLASS);
-                $content.toggleClass(OPEN_CLASS);
+                $toggle.toggleClass(CSS_OPEN_CLASS);
+                $content.toggleClass(CSS_OPEN_CLASS);
             });
         });
     }
 
+    function loadActivePictureInfo(thumb) {
+        var $thumb = $(thumb);
+        var largeUrl = $thumb.data("large");
+        var xlUrl = $thumb.data("xl");
+
+        var $con = $(".main > .image");
+        var $img = $con.hasClass(CSS_EMPTY_CLASS) ? $([]) : $con.find('img');
+
+        if (!$img.length) {
+            $img = $('<img />').attr("src", largeUrl);
+            $con.append($img);
+        }
+        else {
+            updateImage($con, $img, largeUrl);
+        }
+
+
+        $img.one('load', function() {
+            $con.removeClass(CSS_EMPTY_CLASS);
+
+        }).each(function(){
+            if (this.complete) $img.load();
+        });
+    }
+
+    function updateImage(con, img, url) {
+        var $con = $(con);
+        var $img = $(img);
+        var largeUrl = url;
+
+        $img.removeClass(CSS_NEW_CLASS).addClass(CSS_OLD_CLASS);
+        $img = $('<img />').attr("src", largeUrl).addClass(CSS_NEW_CLASS);
+        $con.append($img);
+        $con.find(".old").addClass(CSS_REMOVE_CLASS).delay(500).remove();
+    }
+
+
     function setupProductPhotos() {
         $photoCon.each(function(idx, con) {
             var $con = $(con);
+            var $thumbs = $con.find(".thumbs .image");
 
+            $thumbs.each(function(idx, thumb){
+                var $thumb = $(thumb);
+
+                $thumb.on("click", function(e){
+                    loadActivePictureInfo(this);
+                });
+            });
+
+            $thumbs.first().trigger("click");
         });
     }
 
     function init() {
         setupDescriptionToggle();
+        setupProductPhotos();
+
+        $forms.each(function (idx, form) {
+            var $form = $(form);
+
+            function setupVariantSwitch() {
+                console.log("switch");
+            }
+
+            if (!this.submit_options) {
+                this.submit_options = {};
+            }
+
+            this.submit_options.postUpdate = function () {
+                setupVariantSwitch();
+            };
+        });
     }
 
     init();
