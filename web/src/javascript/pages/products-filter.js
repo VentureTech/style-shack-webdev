55d053e5fc98d02fa61dff2301b3a716462e2fcc
Kaitlyn Noone <knoone@vipasolutions.com>
2015-08-25 17:19:08 -0500

    Adds sort functionality back into page and handles if there is a permalink

--- web/src/javascript/pages/products-filter.js
+++ web/src/javascript/pages/products-filter.js
@@ -1,4 +1,6 @@
 jQuery(function($){
+    var CSS_OPENED_CLASS = ("opened");
+
     $('.filters ul.filter_controls > .mi').each(function() {
         var $con = $(this);
         var $trigger = $con.find('.filter_label');
@@ -19,4 +21,26 @@ jQuery(function($){
             $form.submit();
         });
     });
+
+    $('.sort-by').each(function(){
+        var $trigger = $(this);
+        var $active = $trigger.find(".active-filters > .tag");
+        var $content = $trigger.find('.t2_menu');
+        var $form = $trigger.parents('form');
+
+        if ($active.length > 0) {
+            $trigger.toggleClass(CSS_OPENED_CLASS);
+            $content.toggleClass(CSS_OPENED_CLASS);
+        }
+
+        $trigger.on('click', function() {
+            $trigger.toggleClass(CSS_OPENED_CLASS);
+            $content.toggleClass(CSS_OPENED_CLASS);
+        });
+
+        $content.on('change', 'input[type=radio]', function(evt) {
+            $form.submit();
+        });
+    });
+
 });
