12e18172447a10bf71b6557904529bbf789167f5
Kaitlyn Noone <knoone@vipasolutions.com>
2015-09-01 16:19:17 -0500

    Simplified JS and switched to ajax form update

--- web/src/javascript/pages/products-filter.js
+++ web/src/javascript/pages/products-filter.js
@@ -1,57 +1,74 @@
 jQuery(function($){
     var CSS_OPENED_CLASS = ("opened");
     var $productsCon = $(".products");
+    var $filterCon = $(".filter-con");
+    var $form = $filterCon.find("form");
     var productsWsUrl = $productsCon.data("wsProduct");
+    var filtersWsUrl = $filterCon.data("wsFilters");
     var $productListingCon = $(".product-listing");
+    var $filterListingCon = $(".filter-listing");
 
-    $('.filters ul.filter_controls > .mi').each(function() {
-        var $con = $(this);
-        var $trigger = $con.find('.filter_label');
-        var $content = $con.find('.t2_menu');
-        var $form = $con.parents('form');
 
-        var CSS_OPENED_CLASS = 'opened';
+    function buildUpdater(form, trigger, content, item) {
+        var $form = $(form);
+        var $trigger = $(trigger);
+        var $content = $(content);
+        var $item = $(item) ? $(item) : null;
 
         $trigger.on('click', function(evt) {
-            $con.toggleClass(CSS_OPENED_CLASS);
-            $con.siblings(".mi").removeClass(CSS_OPENED_CLASS);
+            if ($item.length) {
+                $item.siblings("." + CSS_OPENED_CLASS).removeClass(CSS_OPENED_CLASS).find("." + CSS_OPENED_CLASS).removeClass(CSS_OPENED_CLASS);
+                $item.toggleClass(CSS_OPENED_CLASS);
+            }
+            else {
+                $form.find(".search-option").removeClass(CSS_OPENED_CLASS).find("." + CSS_OPENED_CLASS).removeClass(CSS_OPENED_CLASS);
+            }
             $content.toggleClass(CSS_OPENED_CLASS);
-            $con.siblings(".mi").find(".t2_menu").removeClass(CSS_OPENED_CLASS);
         });
 
-        $content.on('change', 'input[type=checkbox]', function(evt) {
-            //$.get(productsWsUrl + "?" + $form.serialize(), function(data){
-            //    $productListingCon.empty();
-            //    $productListingCon.html($($.parseHTML(data)).html());
-            //});
+        $content.on('change', 'input', function(e) {
+            var paramMap = $form.serialize();
 
-            $form.submit();
+            $.get(productsWsUrl + "?" + paramMap, function(data){
+                $productListingCon.empty();
+                $productListingCon.html($($.parseHTML(data)).html());
+            });
+
+            //$form.submit();
         });
 
-        $content.on('change', 'input[type=radio]', function(evt) {
-            $form.submit();
+    }
+
+    $form.each(function(){
+        var $form = $(this);
+        var $filterMenus = $form.find("ul.filter_controls");
+        var $filterMenuItems = $filterMenus.find("> .mi");
+        var $sortMenu = $(".sort-by");
+        var $filterTags = $(".active-filters");
+
+        $filterMenuItems.each(function() {
+            var $item = $(this);
+            var $trigger = $item.find('.filter_label');
+            var $content = $item.find('.t2_menu');
+
+            buildUpdater($form, $trigger, $content, $item);
+        });
+
+        $sortMenu.each(function(){
+            var $trigger = $(this);
+            var $content = $trigger.find(".t2_menu");
+
+            buildUpdater($form, $trigger, $content);
+        });
+
+        $filterTags.click(".tag", function(e){
+            var $tag = $(e.currentTarget);
+            $form.find('#'+$tag.attr("id")).click();
+
+            $.get(productsWsUrl + "?" + $form.serialize(), function(data){
+                $productListingCon.empty();
+                $productListingCon.html($($.parseHTML(data)).html());
+            });
         });
     });
-
-    $('.sort-by').each(function(){
-        var $trigger = $(this);
-        var $active = $trigger.find(".active-filters > .tag");
-        var $content = $trigger.find('.t2_menu');
-        var $form = $trigger.parents('form');
-
-        if ($active.length > 0) {
-            $trigger.toggleClass(CSS_OPENED_CLASS);
-            $content.toggleClass(CSS_OPENED_CLASS);
-        }
-
-        $trigger.on('click', function() {
-            $trigger.toggleClass(CSS_OPENED_CLASS);
-            $content.toggleClass(CSS_OPENED_CLASS);
-        });
-
-        $content.on('change', 'input[type=radio]', function(evt) {
-            $form.submit();
-        });
-    });
-
 });
