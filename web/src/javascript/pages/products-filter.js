b5f8604452fe0f107b6df06ccb938578b3f2e6e1
Kaitlyn Noone <knoone@vipasolutions.com>
2015-09-03 18:27:45 -0500

    All of the changes with Scott's help

--- web/src/javascript/pages/products-filter.js
+++ web/src/javascript/pages/products-filter.js
@@ -1,8 +1,11 @@
 jQuery(function($){
     var CSS_OPENED_CLASS = ("opened");
+    var CSS_ACTIVE_CLASS = "active";
     var $productsCon = $(".products");
     var $filterCon = $(".filter-con");
     var $form = $filterCon.find("form");
+    var isFiltering = false;
+
     var productsWsUrl = $productsCon.data("wsProduct");
     if (productsWsUrl.indexOf("?") > 0) {
         productsWsUrl = productsWsUrl + "&";
@@ -10,64 +13,118 @@ jQuery(function($){
     else {
         productsWsUrl = productsWsUrl + "?";
     }
-    var filtersWsUrl = $filterCon.data("wsFilters");
     var $productListingCon = $(".product-listing");
-    var $filterListingCon = $(".filter-listing");
 
 
-    function buildUpdater(form, trigger, content, item) {
-        var $form = $(form);
-        var $trigger = $(trigger);
-        var $content = $(content);
-        var $item = $(item) ? $(item) : null;
+    function buildUpdater($context) {
+        var $filterGroups = $context.find(".search-option");
 
-        $trigger.on('click', function(evt) {
-            if ($item.length) {
-                $item.siblings("." + CSS_OPENED_CLASS).removeClass(CSS_OPENED_CLASS).find("." + CSS_OPENED_CLASS).removeClass(CSS_OPENED_CLASS);
-                $item.toggleClass(CSS_OPENED_CLASS);
-            }
-            else {
-                $form.find(".search-option").removeClass(CSS_OPENED_CLASS).find("." + CSS_OPENED_CLASS).removeClass(CSS_OPENED_CLASS);
-            }
-            $content.toggleClass(CSS_OPENED_CLASS);
-        });
+        $filterGroups.each(function(idx, filterGroup){
+            var $filterGroup = $(filterGroup);
+            var $trigger = $filterGroup.find(".filter_label");
+            var $filterEls = $filterGroup.find(".mi");
+            var $activeFilterList = $('<div class="active-filters" />').appendTo($filterCon.find(".box"));
 
-        $content.on('change', 'input', function(e) {
-            var paramMap = $form.serialize();
+            var initialActiveFilters = $filterGroup.data("activeFilters") ? $filterGroup.data("activeFilters").split("|") : [];
 
-            $.get(productsWsUrl + paramMap, function(data){
-                $productListingCon.empty();
-                $productListingCon.html($($.parseHTML(data)).html());
-                $productListingCon.trigger("ss:order-stores");
+
+            $trigger.on('click', function(evt) {
+                $filterGroup.siblings("." + CSS_OPENED_CLASS).removeClass(CSS_OPENED_CLASS).find("." + CSS_OPENED_CLASS).removeClass(CSS_OPENED_CLASS);
+                $filterGroup.toggleClass(CSS_OPENED_CLASS);
+            });
+
+            $activeFilterList.on("click", ".tag", function(e){
+                var $tag = $(this);
+                $filterGroup.find('.mi[data-filter-id="' + $tag.data("filterId") + '"]').trigger("ss:filter-inactive");
             });
 
 
-            //$form.submit();
-        });
+            $filterEls.each(function(idx, filterEl) {
+                var $filterEl = $(filterEl);
 
+                function getFilterElById(id) {
+                    return $filterGroup.find('.mi[data-filter-id="' + $filterEl.find("input").val() + '"]')
+                }
+
+                function addToActiveFilterList() {
+                    $('<span class="tag"/>').text($filterEl.find("label").text()).attr("data-filter-id", $filterEl.find("input").val()).appendTo($activeFilterList);
+                }
+
+                function removeFromActiveFilterList() {
+                    $activeFilterList.find('[data-filter-id="' + $filterEl.find("input").val() + '"]').remove();
+                }
+
+                function setFilterActive() {
+                    $filterEl.addClass(CSS_ACTIVE_CLASS);
+                    $filterEl.find("input").prop('checked', true);
+                    addToActiveFilterList();
+                }
+
+                function setFilterInactive() {
+                    $filterEl.removeClass(CSS_ACTIVE_CLASS);
+                    $filterEl.find("input").prop('checked', false);
+                    removeFromActiveFilterList();
+                }
+
+                function toggleFilterState() {
+                    if ($filterEl.hasClass(CSS_ACTIVE_CLASS)) {
+                        setFilterInactive();
+                    }
+                    else {
+                        setFilterActive();
+                    }
+                }
+
+                if ($.inArray($filterEl.data("filterId"), initialActiveFilters) > -1) {
+                    setFilterActive();
+                }
+
+                $filterEl.on("ss:filter-inactive", function(e){
+                    setFilterInactive();
+                });
+
+                $filterEl.on('click', 'label', function(e) {
+                    e.preventDefault();
+
+                    if (isFiltering) {
+                        return;
+                    }
+                    toggleFilterState();
+                    isFiltering = true;
+
+                    loadContent(productsWsUrl + $context.serialize(), function(data) {
+                        renderContent(data);
+                        isFiltering = false;
+                    });
+                });
+            });
+        });
     }
+
+
+
+    function loadContent(url, callback) {
+        $.get(url, callback);
+    }
+
+    function renderContent(content) {
+        $productListingCon.empty();
+        $productListingCon.html($($.parseHTML(content)).html());
+        $productListingCon.trigger("ss:order-stores");
+    }
+
 
     $form.each(function(){
         var $form = $(this);
-        var $filterMenus = $form.find("ul.filter_controls");
-        var $filterMenuItems = $filterMenus.find("> .mi");
-        var $sortMenu = $(".sort-by");
-        var $filterTags = $(".active-filters");
 
-        $filterMenuItems.each(function() {
-            var $item = $(this);
-            var $trigger = $item.find('.filter_label');
-            var $content = $item.find('.t2_menu');
+        buildUpdater($form);
 
-            buildUpdater($form, $trigger, $content, $item);
-        });
-
-        $sortMenu.each(function(){
+        /*$sortMenu.each(function(){
             var $trigger = $(this);
             var $content = $trigger.find(".t2_menu");
 
-            buildUpdater($form, $trigger, $content);
-        });
+            buildUpdater($form, $trigger, $filterMenuItems);
+        });*/
 
         //$filterTags.click(".tag", function(e){
         //    var $tag = $(e.currentTarget);
