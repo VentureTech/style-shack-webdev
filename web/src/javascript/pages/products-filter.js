c607bd5df883bc4b38ccd373ef21cff0d21b6b4f
Scott Benes <sbenes@vipasolutions.com>
2015-09-04 09:05:26 -0500

    Updates to product filter JS

--- web/src/javascript/pages/products-filter.js
+++ web/src/javascript/pages/products-filter.js
@@ -1,50 +1,62 @@
-jQuery(function($){
-    var CSS_OPENED_CLASS = ("opened");
+jQuery(function($) {
+    var CSS_OPENED_CLASS = "opened";
     var CSS_ACTIVE_CLASS = "active";
-    var $productsCon = $(".products");
-    var $filterCon = $(".filter-con");
-    var $form = $filterCon.find("form");
-    var isFiltering = false;
 
-    var productsWsUrl = $productsCon.data("wsProduct");
-    if (productsWsUrl.indexOf("?") > 0) {
-        productsWsUrl = productsWsUrl + "&";
-    }
-    else {
-        productsWsUrl = productsWsUrl + "?";
-    }
+    var $body = $('body');
+    var $root = $('#e-content');
     var $productListingCon = $(".product-listing");
 
+    var productsWsUrl = $productListingCon.find('.products').data("wsProduct");
 
-    function buildUpdater($context) {
+    var isLoadingContent = false;
+
+
+    function initFilterCon($context) {
+        var $filterCon = $context;
         var $filterGroups = $context.find(".search-option");
+        var $filterForm = $context.find('form');
+
+        $root.on('ss:filter-groups-close', function() {
+            $filterGroups.trigger('ss:filter-group-close');
+        });
 
         $filterGroups.each(function(idx, filterGroup){
             var $filterGroup = $(filterGroup);
-            var $trigger = $filterGroup.find(".filter_label");
+            var $filterGroupLabel = $filterGroup.find(".filter_label");
             var $filterEls = $filterGroup.find(".mi");
             var $activeFilterList = $('<div class="active-filters" />').appendTo($filterCon.find(".box"));
 
             var initialActiveFilters = $filterGroup.data("activeFilters") ? $filterGroup.data("activeFilters").split("|") : [];
 
+            function $getFilterElById(id) {
+                return $filterGroup.find('.mi[data-filter-id="' + id + '"]')
+            }
 
-            $trigger.on('click', function(evt) {
-                $filterGroup.siblings("." + CSS_OPENED_CLASS).removeClass(CSS_OPENED_CLASS).find("." + CSS_OPENED_CLASS).removeClass(CSS_OPENED_CLASS);
-                $filterGroup.toggleClass(CSS_OPENED_CLASS);
+            $filterGroup.on('ss:filter-group-open', function() {
+                $filterGroup.addClass(CSS_OPENED_CLASS);
             });
 
-            $activeFilterList.on("click", ".tag", function(e){
-                var $tag = $(this);
-                $filterGroup.find('.mi[data-filter-id="' + $tag.data("filterId") + '"]').trigger("ss:filter-inactive");
+            $filterGroup.on('ss:filter-group-close', function() {
+                $filterGroup.removeClass(CSS_OPENED_CLASS);
+            });
+
+            $filterGroupLabel.on('click', function(evt) {
+                evt.stopPropagation();
+
+                if ($filterGroup.hasClass(CSS_OPENED_CLASS)) {
+                    $filterGroup.trigger('ss:filter-group-close');
+                } else {
+                    $filterGroup.trigger('ss:filter-group-open').siblings().trigger('ss:filter-group-close');
+                }
+            });
+
+            $activeFilterList.on("click", ".tag", function(e) {
+                $getFilterElById($(this).data("filterId")).trigger("ss:filter-inactive");
             });
 
 
             $filterEls.each(function(idx, filterEl) {
                 var $filterEl = $(filterEl);
-
-                function getFilterElById(id) {
-                    return $filterGroup.find('.mi[data-filter-id="' + $filterEl.find("input").val() + '"]')
-                }
 
                 function addToActiveFilterList() {
                     $('<span class="tag"/>').text($filterEl.find("label").text()).attr("data-filter-id", $filterEl.find("input").val()).appendTo($activeFilterList);
@@ -58,12 +70,14 @@ jQuery(function($){
                     $filterEl.addClass(CSS_ACTIVE_CLASS);
                     $filterEl.find("input").prop('checked', true);
                     addToActiveFilterList();
+                    $filterForm.trigger('ss:filter-update');
                 }
 
                 function setFilterInactive() {
                     $filterEl.removeClass(CSS_ACTIVE_CLASS);
                     $filterEl.find("input").prop('checked', false);
                     removeFromActiveFilterList();
+                    $filterForm.trigger('ss:filter-update');
                 }
 
                 function toggleFilterState() {
@@ -85,55 +99,119 @@ jQuery(function($){
 
                 $filterEl.on('click', 'label', function(e) {
                     e.preventDefault();
+                    e.stopPropagation();
 
-                    if (isFiltering) {
+                    if (isLoadingContent) {
                         return;
                     }
-                    toggleFilterState();
-                    isFiltering = true;
 
-                    loadContent(productsWsUrl + $context.serialize(), function(data) {
-                        renderContent(data);
-                        isFiltering = false;
-                    });
+                    toggleFilterState();
                 });
             });
         });
     }
 
+    function initSortCon($context) {
+        var $sortItems = $context.find('.mi');
+        var $sortInputs = $context.find('input');
+        var $form = $context.closest('form');
 
+        var activeSortKey = '';
 
-    function loadContent(url, callback) {
-        $.get(url, callback);
+        $root.on('ss:sort-close', function() {
+            $context.removeClass(CSS_OPENED_CLASS);
+        });
+
+        $root.on('ss:sort-open', function() {
+            $context.addClass(CSS_OPENED_CLASS);
+        });
+
+        $context.on('click', '.label', function(evt) {
+            evt.stopPropagation();
+
+            if ($context.hasClass(CSS_OPENED_CLASS)) {
+                $context.trigger('ss:sort-close');
+            } else {
+                $context.trigger('ss:sort-open');
+            }
+        });
+
+        $context.on('click', '.mi', function() {
+            var $sortItem = $(this);
+            var $sortInput = $sortItem.find("input");
+            var sortInputKey = $sortInput.val();
+
+            if (sortInputKey == activeSortKey) {
+                return;
+            }
+
+            $sortItems.removeClass(CSS_ACTIVE_CLASS);
+            $sortInputs.prop('checked', false);
+
+            $sortItem.addClass(CSS_ACTIVE_CLASS);
+            $sortInput.prop('checked', true);
+
+            activeSortKey = sortInputKey;
+            $form.trigger('ss:sort-update');
+        });
+    }
+
+    function loadContent(url) {
+        return $.get(url);
     }
 
     function renderContent(content) {
         $productListingCon.empty();
-        $productListingCon.html($($.parseHTML(content)).html());
-        $productListingCon.trigger("ss:order-stores");
+        $productListingCon.html($(content).html());
     }
 
+    function getFilteredContentUrl($form) {
+        return productsWsUrl + (productsWsUrl.indexOf('?') > -1 ? '&' : '?') + $form.serialize();
+    }
 
-    $form.each(function(){
-        var $form = $(this);
+    function init() {
+        $root.find('.filter-con').each(function() {
+            initFilterCon($(this));
+        });
 
-        buildUpdater($form);
+        $root.find('.sort-by').each(function() {
+            initSortCon($(this));
+        });
 
-        /*$sortMenu.each(function(){
-            var $trigger = $(this);
-            var $content = $trigger.find(".t2_menu");
+        $root.on('ss:content-load', function(evt, url) {
+            $root.trigger('ss:content-loading');
 
-            buildUpdater($form, $trigger, $filterMenuItems);
-        });*/
+            $.when(loadContent(url)).then(function(data) {
+                $root.trigger('ss:content-loaded', $.parseHTML(data));
+            });
+        });
 
-        //$filterTags.click(".tag", function(e){
-        //    var $tag = $(e.currentTarget);
-        //    $form.find('#'+$tag.attr("id")).click();
-        //
-        //    $.get(productsWsUrl + "?" + $form.serialize(), function(data){
-        //        $productListingCon.empty();
-        //        $productListingCon.html($($.parseHTML(data)).html());
-        //    });
-        //});
-    });
+        $root.on('ss:content-loading', function() {
+            isLoadingContent = true;
+        });
+
+        $root.on('ss:content-loaded', function(evt, content) {
+            renderContent(content);
+            //$content.trigger("ss:order-stores");
+            isLoadingContent = false;
+        });
+
+        $root.on('ss:filter-update ss:sort-update', 'form', function() {
+            $root.trigger('ss:content-load', getFilteredContentUrl($(this)));
+        });
+
+        $root.on('ss:filter-group-open', function() {
+            $root.trigger('ss:sort-close');
+        });
+
+        $root.on('ss:sort-open', function() {
+            $root.trigger('ss:filter-groups-close');
+        });
+
+        $body.on('click', function() {
+            $root.trigger('ss:sort-close').trigger('ss:filter-groups-close');
+        });
+    }
+
+    init();
 });
