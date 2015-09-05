06cdeb7c9cd179014148a57f74d157ab42f479ff
Scott Benes <sbenes@vipasolutions.com>
2015-09-05 18:44:38 -0500

    Update menu from ID to class

--- web/src/javascript/components/blog-filters.js
+++ web/src/javascript/components/blog-filters.js
@@ -2,7 +2,7 @@
  * Created by webdev on 8/7/15.
  */
 jQuery(function($){
-    var $mainNav = $("#main-nav");
+    var $mainNav = $(".main-nav");
     var $spotlightCon =  $('.spotlight-trends');
 
     $mainNav.find(".mi.trends").append($spotlightCon);
