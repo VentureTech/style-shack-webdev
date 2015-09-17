1dc62bd18cf587b36034e9d965c9776cc72d2260
Kaitlyn Noone <knoone@vipasolutions.com>
2015-09-17 17:01:38 -0500

    Initial commit of new files

---
+++ web/src/javascript/pages/store-geolocations.js
@@ -0,0 +1,69 @@
+var isiPad = /ipad/i.test(navigator.userAgent.toLowerCase());
+var isiPhone = /iphone/i.test(navigator.userAgent.toLowerCase());
+var isiPod = /ipod/i.test(navigator.userAgent.toLowerCase());
+var isiDevice = /ipad|iphone|ipod/i.test(navigator.userAgent.toLowerCase());
+var isAndroid = /android/i.test(navigator.userAgent.toLowerCase());
+var isBlackBerry = /blackberry/i.test(navigator.userAgent.toLowerCase());
+var isWebOS = /webos/i.test(navigator.userAgent.toLowerCase());
+var isWindowsPhone = /windows phone/i.test(navigator.userAgent.toLowerCase());
+
+if (!isiPad && !isiPhone && !isiPod  && !isiDevice && !isAndroid && !isBlackBerry && !isWebOS && !isWindowsPhone && jQuery(window).width() > 1025) {
+    window.location.href="/locations/search";
+}
+
+jQuery(function($){
+    var $fail = $('.geo-fail');
+    var $wait = $('.geo-wait');
+    var $timedOut = $('.geo-timeout');
+    var $success = $('.geo-results');
+    var timeoutFail;
+    var dataURL = $('.geo-results').attr("data-url");
+
+    var fail = function(timeout) {
+        var timeout = timeout;
+        $wait.hide();
+        if (timeout) {
+            $timedOut.show();
+        }
+        else {
+            $fail.show();
+        }
+    };
+
+    var waiting = function(){
+        timeoutFail = setTimeout(function(){
+            fail("timeout");
+            clearTimeout(timeoutFail);
+        },10000);
+
+        $fail.hide();
+        $timedOut.hide();
+        $wait.show();
+    };
+
+    var success = function(position) {
+        clearTimeout(timeoutFail);
+        var params = {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+            distance: 50
+        };
+
+        $success
+            .find('.listing')
+            .load(dataURL, params, function(){
+                $wait.hide();
+                $timedOut.hide();
+                $success.show();
+
+            });
+    };
+
+    if (navigator.geolocation) {
+        waiting();
+        navigator.geolocation.getCurrentPosition(success, fail);
+    } else {
+        fail();
+    }
+});
+
