86cae91b2ba9fa27f5a054b6a315bab5e09a6355
Kaitlyn Noone <knoone@vipasolutions.com>
2015-09-21 16:49:36 -0500

    Removes redirect check to different file

--- web/src/javascript/pages/store-geolocations.js
+++ web/src/javascript/pages/store-geolocations.js
@@ -1,23 +1,10 @@
-var isiPad = /ipad/i.test(navigator.userAgent.toLowerCase());
-var isiPhone = /iphone/i.test(navigator.userAgent.toLowerCase());
-var isiPod = /ipod/i.test(navigator.userAgent.toLowerCase());
-var isiDevice = /ipad|iphone|ipod/i.test(navigator.userAgent.toLowerCase());
-var isAndroid = /android/i.test(navigator.userAgent.toLowerCase());
-var isBlackBerry = /blackberry/i.test(navigator.userAgent.toLowerCase());
-var isWebOS = /webos/i.test(navigator.userAgent.toLowerCase());
-var isWindowsPhone = /windows phone/i.test(navigator.userAgent.toLowerCase());
-
-if (!isiPad && !isiPhone && !isiPod  && !isiDevice && !isAndroid && !isBlackBerry && !isWebOS && !isWindowsPhone && jQuery(window).width() > 1025) {
-    window.location.href="/locations/search";
-}
-
 jQuery(function($){
     var $fail = $('.geo-fail');
     var $wait = $('.geo-wait');
     var $timedOut = $('.geo-timeout');
     var $success = $('.geo-results');
     var timeoutFail;
-    var dataURL = $('.geo-results').attr("data-url");
+    var dataURL = $success.attr("data-url");
 
     var fail = function(timeout) {
         var timeout = timeout;
