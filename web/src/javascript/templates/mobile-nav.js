d72b5ac09e7d91f446b670becb35c1a656a4568f
Kaitlyn Noone <knoone@vipasolutions.com>
2015-09-22 17:07:17 -0500

    Adds hover and touch functionality for submenus on dropdown

--- web/src/javascript/templates/mobile-nav.js
+++ web/src/javascript/templates/mobile-nav.js
@@ -1,4 +1,5 @@
 jQuery(function($){
+    var $window = $(window);
     var $navTrigger = $('<div class="mobile-nav-trigger"><div class="line" /><div class="line" /><div class="line" /></div>');
     var $shackTrigger = $('<div class="shack-nav-trigger" />');
     var $primaryMobileNav = $('<div class="mobile-main" />');
@@ -6,26 +7,19 @@ jQuery(function($){
 
     var $header = $(".l-top-nav > .box-wc");
     var $loginMenuItems = $header.find(".menu-comp .mi").not('.shopping-bag');
-    var $mobileLoginMenu = $('<ul class="menu"/>').append($loginMenuItems.clone());
+    var $mobileLoginMenu = $('<ul class="menu-t1"/>').append($loginMenuItems.clone());
     var $mainMenu = $(".main-nav > .menu");
     var $userMenu = $(".user-profile > .menu");
+
+    var CSS_ACTIVE_CLASS = "active";
     var CSS_OPEN_CLASS = "open";
+    var BREAKPOINT_WIDTH_TOUCH = 767;
+    var RESIZE_THROTTLE_TIME = 200;
 
+    var resizeThrottleId;
+    var windowWidth = 0;
 
-
-    function setupMobileNav() {
-        $header.append($navTrigger);
-        $header.append($shackTrigger);
-        $primaryMobileNav.append($mainMenu.clone());
-        $primaryMobileNav.append($userMenu.clone());
-        $loginMobileNav.append($mobileLoginMenu);
-        $header.append($primaryMobileNav);
-        $header.append($loginMobileNav);
-
-        setupNavToggles();
-    }
-
-
+    var isTouch = (Modernizr && Modernizr.touch) || navigator.maxTouchPoints;
 
     function setupNavToggles() {
         var $navToggle = $(".mobile-nav-trigger");
@@ -60,6 +54,118 @@ jQuery(function($){
         });
     }
 
-    setupMobileNav();
+    function setupMobileHeader() {
+        $header.append($navTrigger);
+        $header.append($shackTrigger);
+        $primaryMobileNav.append($mainMenu.clone());
+        $primaryMobileNav.append($userMenu.clone());
+        $loginMobileNav.append($mobileLoginMenu);
+        $header.append($primaryMobileNav);
+        $header.append($loginMobileNav);
+
+        setupNavToggles();
+    }
+
+
+    function initNav() {
+        setupMobileHeader();
+
+        $header.find(".menu").each(function(idx, menu) {
+            var $menu = $(menu);
+            var $parentMenuItems = $menu.find(".mi-parent");
+
+            $parentMenuItems.each(function(idx, item){
+                var $mi = $(item);
+                $mi.prepend('<div class="collapse"><span></span></div>');
+            });
+
+            function addHoverEvents() {
+                $menu.on('mouseenter', '.mi-parent', function(evt) {
+                    if (isTouch && windowWidth <= BREAKPOINT_WIDTH_TOUCH) {
+                        return;
+                    }
+
+                    $(this).addClass(CSS_OPEN_CLASS).siblings().removeClass(CSS_ACTIVE_CLASS);
+                });
+
+                $menu.on('mouseleave', '.mi-parent', function(evt) {
+                    var $mi = $(this);
+
+                    if (isTouch && windowWidth <= BREAKPOINT_WIDTH_TOUCH) {
+                        return;
+                    }
+
+                    $mi.removeClass(CSS_OPEN_CLASS);
+                });
+            }
+
+            function addTouchEvents() {
+                $menu.on('click', '.mi-parent', function(evt) {
+                    var $mi = $(this);
+
+                    if (isTouch && windowWidth > BREAKPOINT_WIDTH_TOUCH && !$mi.hasClass(CSS_OPEN_CLASS)) {
+                        evt.preventDefault();
+                        $mi.addClass(CSS_OPEN_CLASS).siblings().removeClass(CSS_ACTIVE_CLASS);
+
+                    }
+
+                    if (windowWidth > BREAKPOINT_WIDTH_TOUCH) {
+                        return;
+                    }
+
+                    if (!$mi.hasClass(CSS_OPEN_CLASS)) {
+                        evt.preventDefault();
+
+                        $parentMenuItems.removeClass(CSS_OPEN_CLASS);
+                        $mi.addClass(CSS_OPEN_CLASS);
+                    }
+                });
+
+                $parentMenuItems.on("click", ".collapse", function(evt) {
+                    var $toggle = $(this);
+                    var $parent = $toggle.closest(".mi");
+                    if ($parent.hasClass(CSS_OPEN_CLASS)) {
+                        evt.stopPropagation();
+
+                        if ($parent.hasClass(CSS_OPEN_CLASS)) {
+                            evt.preventDefault();
+
+                            $parentMenuItems.removeClass(CSS_OPEN_CLASS);
+                            $parent.removeClass(CSS_OPEN_CLASS);
+                        }
+                    }
+
+                });
+            }
+
+            function updateWindowWidth() {
+                windowWidth = $window.width();
+            }
+
+            function addResizeWatcher() {
+                $window.on('resize', function() {
+                    clearTimeout(resizeThrottleId);
+                    resizeThrottleId = setTimeout(function() {
+                        updateWindowWidth();
+                    }, RESIZE_THROTTLE_TIME);
+                });
+            }
+
+            updateWindowWidth();
+
+            if (isTouch) {
+                console.log("touch");
+                addTouchEvents();
+                addHoverEvents();
+                addResizeWatcher();
+            } else {
+                addHoverEvents();
+            }
+        });
+    }
+
+
+
+    initNav();
 
 });
