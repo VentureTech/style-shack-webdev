b9af7fc0e6d8120b65445e13aa387524df65f306
Kaitlyn Noone <knoone@vipasolutions.com>
2015-09-23 17:27:36 -0500

    Additional changes to get it working

--- web/src/javascript/templates/mobile-nav.js
+++ web/src/javascript/templates/mobile-nav.js
@@ -7,9 +7,12 @@ jQuery(function($){
 
     var $header = $(".l-top-nav > .box-wc");
     var $loginMenuItems = $header.find(".menu-comp .mi").not('.shopping-bag');
-    var $mobileLoginMenu = $('<ul class="menu-t1"/>').append($loginMenuItems.clone());
+    var $mobileLoginMenu = $('<ul class="menu-t1 menu"/>').append($loginMenuItems.clone());
     var $mainMenu = $(".main-nav > .menu");
     var $userMenu = $(".user-profile > .menu");
+
+    var $mobileNav,
+        $loginNav;
 
     var CSS_ACTIVE_CLASS = "active";
     var CSS_OPEN_CLASS = "open";
@@ -21,39 +24,8 @@ jQuery(function($){
 
     var isTouch = (Modernizr && Modernizr.touch) || navigator.maxTouchPoints;
 
-    function setupNavToggles() {
-        var $navToggle = $(".mobile-nav-trigger");
-        var $shackToggle = $(".shack-nav-trigger");
-        var $mobileNav = $(".mobile-main");
-        var $loginNav = $(".login-main");
-
-        $navToggle.on("click", function(e){
-            if ($loginNav.hasClass(CSS_OPEN_CLASS)) {
-                $loginNav.toggleClass(CSS_OPEN_CLASS);
-                setTimeout(function(){
-                    $mobileNav.toggleClass(CSS_OPEN_CLASS);
-                },500);
-
-            }
-            else {
-                $mobileNav.toggleClass(CSS_OPEN_CLASS);
-            }
-        });
-
-        $shackToggle.on("click", function(e){
-            if ($mobileNav.hasClass(CSS_OPEN_CLASS)) {
-                $mobileNav.toggleClass(CSS_OPEN_CLASS);
-                setTimeout(function(){
-                    $loginNav.toggleClass(CSS_OPEN_CLASS);
-                },500);
-
-            }
-            else {
-                $loginNav.toggleClass(CSS_OPEN_CLASS);
-            }
-        });
-    }
-
+    /* Append new toggle elements to top header. Clone menus out of current location
+    and into new menu in top header. Trigger toggle setup **/
     function setupMobileHeader() {
         $header.append($navTrigger);
         $header.append($shackTrigger);
@@ -66,20 +38,61 @@ jQuery(function($){
         setupNavToggles();
     }
 
+    /* Create new object reference to toggle and new menus. Add click handler for toggles. **/
+    function setupNavToggles() {
+        var $navToggle = $(".mobile-nav-trigger");
+        var $shackToggle = $(".shack-nav-trigger");
+        $mobileNav = $(".mobile-main");
+        $loginNav = $(".login-main");
+
+        $navToggle.on("click", function(e){
+            toggleClick($mobileNav, $loginNav)
+        });
+
+        $shackToggle.on("click", function(e){
+            toggleClick($loginNav, $mobileNav)
+        });
+    }
+
+    /* Check to see if other top menu is open on click. If so, close it and
+    delay the animated opening of the target menu **/
+    function toggleClick(targetMenu, otherMenu) {
+        var $menu = $(targetMenu);
+        var $otherMenu = $(otherMenu);
+
+
+        if ($otherMenu.hasClass(CSS_OPEN_CLASS)) {
+            $otherMenu.toggleClass(CSS_OPEN_CLASS);
+            setTimeout(function(){
+                $menu.toggleClass(CSS_OPEN_CLASS);
+            },500);
+
+        }
+        else {
+            $menu.toggleClass(CSS_OPEN_CLASS);
+        }
+    }
+
 
     function initNav() {
         setupMobileHeader();
 
-        $header.find(".menu").each(function(idx, menu) {
+        /* Find top level menus in top header and do work **/
+        $mobileNav.add($loginNav).each(function(idx, menu) {
             var $menu = $(menu);
             var $parentMenuItems = $menu.find(".mi-parent");
 
+
+            /* Append collapse element to help handle 2-click submenu system **/
             $parentMenuItems.each(function(idx, item){
                 var $mi = $(item);
                 $mi.prepend('<div class="collapse"><span></span></div>');
             });
 
+
+            /* Add hover events for non-touch clients **/
             function addHoverEvents() {
+                /* on hover */
                 $menu.on('mouseenter', '.mi-parent', function(evt) {
                     if (isTouch && windowWidth <= BREAKPOINT_WIDTH_TOUCH) {
                         return;
@@ -88,6 +101,7 @@ jQuery(function($){
                     $(this).addClass(CSS_OPEN_CLASS).siblings().removeClass(CSS_ACTIVE_CLASS);
                 });
 
+                /* on hover out */
                 $menu.on('mouseleave', '.mi-parent', function(evt) {
                     var $mi = $(this);
 
@@ -99,28 +113,24 @@ jQuery(function($){
                 });
             }
 
+
+            /* Add touch events for touch clients **/
             function addTouchEvents() {
                 $menu.on('click', '.mi-parent', function(evt) {
                     var $mi = $(this);
 
-                    if (isTouch && windowWidth > BREAKPOINT_WIDTH_TOUCH && !$mi.hasClass(CSS_OPEN_CLASS)) {
+                    /* if is touch and menu item isn't already open,
+                    * don't go anywhere with the click and open the item and close the other opens siblings */
+                    if (isTouch && !$mi.hasClass(CSS_OPEN_CLASS)) {
                         evt.preventDefault();
                         $mi.addClass(CSS_OPEN_CLASS).siblings().removeClass(CSS_ACTIVE_CLASS);
-
-                    }
-
-                    if (windowWidth > BREAKPOINT_WIDTH_TOUCH) {
-                        return;
-                    }
-
-                    if (!$mi.hasClass(CSS_OPEN_CLASS)) {
-                        evt.preventDefault();
-
-                        $parentMenuItems.removeClass(CSS_OPEN_CLASS);
-                        $mi.addClass(CSS_OPEN_CLASS);
                     }
                 });
 
+
+
+                /* on click on the collapse div, find the containing menu item
+                 * and do not execute the link, but open/close the menu item */
                 $parentMenuItems.on("click", ".collapse", function(evt) {
                     var $toggle = $(this);
                     var $parent = $toggle.closest(".mi");
@@ -154,7 +164,6 @@ jQuery(function($){
             updateWindowWidth();
 
             if (isTouch) {
-                console.log("touch");
                 addTouchEvents();
                 addHoverEvents();
                 addResizeWatcher();
