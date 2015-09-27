aebab1870f5658f4a4e60ac80d79bc72ca54cf77
Kaitlyn <knoone@vipasolutions.com>
2015-09-27 16:34:05 -0500

    Updates UI to destroy and rebuild if the display breakpoint changes. Also adds functionality to change price on variant change.

--- web/src/javascript/pages/product-detail.js
+++ web/src/javascript/pages/product-detail.js
@@ -1,4 +1,4 @@
-jQuery(function($){
+jQuery(function ($) {
     var CSS_OPEN_CLASS = "open";
     var CSS_PREVIOUS_CLASS = "prev";
     var CSS_NEXT_CLASS = "next";
@@ -15,15 +15,15 @@ jQuery(function($){
     var $bodyCon = $("body");
     var $productVariantManager = $("#variant-mgr-id");
     var $infoCon = $(".more-info > div");
-    var $loadingDiv = $('<div class="loading" />');
     var $photoCon = $(".images");
-    var $forms = $("form");
     var $nav = $('<ul class="nav" />');
     var $prev = $('<div class="navi prev"><span></span></div>').addClass(CSS_ACTIVE_CLASS).appendTo($nav);
     var $next = $('<div class="navi next"><span></span></div>').addClass(CSS_ACTIVE_CLASS).appendTo($nav);
     var $wrapper = $('<div class="wrapper" />');
     var $hScrollpane = $('<div class="horizontal-scrollpane" />');
     var $vScrollpane = $('<div class="vertical-scrollpane" />');
+    var $variantConWrap = $('<div class="variants collection" />');
+    var $overallConWrap = $('<div class="overall collection" />');
 
 
     var resizeThrottleId;
@@ -31,24 +31,39 @@ jQuery(function($){
 
     var $window = $(window);
     var windowWidth = $window.width();
+    var prevWindowWidth;
 
     var isTouch = (Modernizr && Modernizr.touch) || navigator.maxTouchPoints;
 
     function updateWindowWidth() {
+        prevWindowWidth = windowWidth;
         windowWidth = $window.width();
+
+        if (windowWidth < BREAKPOINT_TABLET_PORTRAIT && prevWindowWidth > BREAKPOINT_TABLET_PORTRAIT) {
+            $bodyCon.trigger("ss:pvm-loaded");
+        }
+
+        if (windowWidth > BREAKPOINT_TABLET_PORTRAIT && prevWindowWidth < BREAKPOINT_TABLET_PORTRAIT) {
+            $bodyCon.trigger("ss:pvm-loaded");
+        }
     }
 
     function addResizeWatcher() {
-        $window.on('resize', function() {
+        $window.on('resize', function () {
             clearTimeout(resizeThrottleId);
-            resizeThrottleId = setTimeout(function() {
+            resizeThrottleId = setTimeout(function () {
                 updateWindowWidth();
             }, RESIZE_THROTTLE_TIME);
         });
     }
 
 
-    function createThumbFunctionality() {
+    function createDesktopThumbFunctionality() {
+        var $variantThumbs = $photoCon.find(".thumbs .variant-img");
+        var $overallThumbs = $photoCon.find(".thumbs .overall-img");
+        $variantThumbs.wrapAll($variantConWrap);
+        $overallThumbs.wrapAll($overallConWrap);
+
         var $overallCon = $photoCon.find(".overall");
         var $overallWrapper,
             $overallScrollpane;
@@ -61,7 +76,7 @@ jQuery(function($){
         var newTransitionValue;
         var thumbCount = $overallCon.find(".image").length;
 
-        function setupThumbWrapper() {
+        function setupThumbWrappers() {
             if (thumbCount > MAX_THUMBS_VISIBLE) {
                 $overallCon.wrapInner($wrapper);
                 $overallWrapper = $overallCon.find(".wrapper");
@@ -70,6 +85,7 @@ jQuery(function($){
                 $overallCon.append($nav);
                 $nextBtn = $overallCon.find(".next");
                 $prevBtn = $overallCon.find(".prev");
+                updateThumbPaging();
             }
         }
 
@@ -100,7 +116,7 @@ jQuery(function($){
             $nextBtn.addClass(CSS_ACTIVE_CLASS);
             $prevBtn.addClass(CSS_ACTIVE_CLASS);
 
-            if ((thumbCount - MAX_THUMBS_VISIBLE*thumbPageCount) < MAX_THUMBS_VISIBLE) {
+            if ((thumbCount - MAX_THUMBS_VISIBLE * thumbPageCount) < MAX_THUMBS_VISIBLE) {
                 $nextBtn.removeClass(CSS_ACTIVE_CLASS);
             }
             if (thumbPageCount == 0) {
@@ -109,14 +125,14 @@ jQuery(function($){
         }
 
 
-        setupThumbWrapper();
+        setupThumbWrappers();
 
 
-        $overallCon.on('click', '.next.active', function(e){
+        $overallCon.on('click', '.next.active', function (e) {
             scrollThumbs('down');
         });
 
-        $overallCon.on('click', '.prev.active', function(e){
+        $overallCon.on('click', '.prev.active', function (e) {
             scrollThumbs('up');
         });
     }
@@ -137,45 +153,12 @@ jQuery(function($){
 
     function setupThumbs() {
         if (windowWidth > BREAKPOINT_TABLET_PORTRAIT) {
-            createThumbFunctionality();
+            createDesktopThumbFunctionality();
         }
         else {
             setupMobileSlides();
         }
     }
-
-    function updateImage(url) {
-        var $image = $photoCon.find(".main .image");
-        var largeUrl = url;
-
-        $image.css('background-image', 'url(' + largeUrl + ')');
-    }
-
-    function setupProductPhotos() {
-        if (windowWidth < BREAKPOINT_TABLET_PORTRAIT) {
-            $photoCon.find('.image').off();
-            $photoCon.find('.main').empty();
-            setupThumbs();
-        }
-        else {
-            $photoCon.each(function (idx, con) {
-                var $con = $(con);
-                var $thumbs = $con.find(".thumbs .image");
-
-                $thumbs.each(function (idx, thumb) {
-                    var $thumb = $(thumb);
-                    var largeUrl = $thumb.data("large");
-
-                    $thumb.on("click", function(e) {
-                        updateImage(largeUrl);
-                    });
-                });
-
-                $thumbs.first().trigger("click");
-            });
-        }
-    }
-
 
     function setupMobileSlides() {
         var $thumbCon = $photoCon.find(".thumbs");
@@ -191,6 +174,9 @@ jQuery(function($){
         if (isTouch) {
             $thumbCon.addClass("mobi");
         }
+        else {
+            $thumbCon.removeClass("mobi");
+        }
 
         $nav.prependTo($thumbCon);
         $nav.wrap('<div class="nav_con" />');
@@ -200,23 +186,23 @@ jQuery(function($){
         slideWidth = $slides.width();
 
 
-        $nav.on('click', '.navi.next', function(evt){
+        $nav.on('click', '.navi.next', function (evt) {
             nextSlide("left");
         });
 
-        $nav.on('click', '.navi.prev', function(evt){
+        $nav.on('click', '.navi.prev', function (evt) {
             nextSlide("right");
         });
 
-        $thumbCon.on("swipeleft", function(event) {
+        $thumbCon.on("swipeleft", function (event) {
             nextSlide("left");
         });
 
-        $thumbCon.on("swiperight", function(event) {
+        $thumbCon.on("swiperight", function (event) {
             nextSlide("right");
         });
 
-        $thumbCon.on("movestart", function(e) {
+        $thumbCon.on("movestart", function (e) {
             // allows swipe up and down on mobile
             if ((e.distX > e.distY && e.distX < -e.distY) ||
                 (e.distX < e.distY && e.distX > -e.distY)) {
@@ -240,7 +226,7 @@ jQuery(function($){
         }
 
         function moveSlide() {
-            var transitionValue = -(slideWidth*curSlideIdx);
+            var transitionValue = -(slideWidth * curSlideIdx);
             if (Modernizr.csstransforms) {
                 transitionValue = "translateX(" + transitionValue + "px)";
             }
@@ -258,6 +244,52 @@ jQuery(function($){
         }
     }
 
+    function updateImage(url) {
+        var $image = $photoCon.find(".main .image");
+        var largeUrl = url;
+
+        $image.css('background-image', 'url(' + largeUrl + ')');
+    }
+
+    function setupProductPhotos() {
+        $photoCon.find('.image').off();
+        $photoCon.find(".navi").off();
+        setupThumbs();
+
+        $photoCon.each(function (idx, con) {
+            var $con = $(con);
+            var $thumbs = $con.find(".thumbs .image");
+
+            $thumbs.each(function (idx, thumb) {
+                var $thumb = $(thumb);
+                var largeUrl = $thumb.data("large");
+
+                $thumb.on("click", function (e) {
+                    updateImage(largeUrl);
+                });
+            });
+
+            $thumbs.first().trigger("click");
+        });
+    }
+
+
+    function updateVariantPrice(variantID) {
+        var updateURL = $(".price-manager").data('updateUrl');
+        var productID = $(".price-manager").data('productId');
+        var updateData = {
+            product: productID,
+            variant: variantID
+        };
+
+        $.get(updateURL, updateData, function (html) {
+            var $newNodes = $($.parseHTML(html));
+            $('.price-manager').empty().append($newNodes.find(".price-manager").text().trim());
+        });
+    }
+
+
+
 
     function updateImageManager(variantID) {
         var updateURL = $(".image-manager").data('updateUrl');
@@ -267,7 +299,7 @@ jQuery(function($){
             variant: variantID
         };
 
-        $.get(updateURL, updateData, function(html){
+        $.get(updateURL, updateData, function (html) {
             var $newNodes = $($.parseHTML(html));
             $('.image-manager').empty().append($newNodes.find(".image-manager").children());
             setupProductPhotos();
@@ -275,25 +307,19 @@ jQuery(function($){
     }
 
 
-
     function init() {
         setupDescriptionToggle();
         addResizeWatcher();
         setupProductPhotos();
 
-        $bodyCon.on("ss:pvm-loading", function(e){
+        $bodyCon.on("ss:pvm-loading", function (e) {
             $photoCon.find('.image').off();
-            if ($photoCon.find(".loaded").length >= 0) {
-                $photoCon.append($loadingDiv);
-            }
-            $photoCon.find(".main").empty();
-            $photoCon.find(".thumbs").empty();
         });
 
-        $bodyCon.on("ss:pvm-loaded", function(e){
+        $bodyCon.on("ss:pvm-loaded", function (e) {
             var productVariantID = $productVariantManager.find("> div").data("pvId");
             updateImageManager(productVariantID);
-            $photoCon.find(".loading").remove();
+            updateVariantPrice(productVariantID);
         });
     }
 
