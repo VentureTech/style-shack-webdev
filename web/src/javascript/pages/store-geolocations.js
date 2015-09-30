cee3c83266a4a2cebf919afac1f1f0c93318f38e
Kaitlyn Noone <knoone@vipasolutions.com>
2015-09-30 17:32:52 -0500

    Changes to how pages redirect to one another

--- web/src/javascript/pages/store-geolocations.js
+++ web/src/javascript/pages/store-geolocations.js
@@ -1,3 +1,18 @@
+var isiPad = /ipad/i.test(navigator.userAgent.toLowerCase());
+var isiPhone = /iphone/i.test(navigator.userAgent.toLowerCase());
+var isiPod = /ipod/i.test(navigator.userAgent.toLowerCase());
+var isiDevice = /ipad|iphone|ipod/i.test(navigator.userAgent.toLowerCase());
+var isAndroid = /android/i.test(navigator.userAgent.toLowerCase());
+var isBlackBerry = /blackberry/i.test(navigator.userAgent.toLowerCase());
+var isWebOS = /webos/i.test(navigator.userAgent.toLowerCase());
+var isWindowsPhone = /windows phone/i.test(navigator.userAgent.toLowerCase());
+
+var params = (window.location.search);
+
+if (!(isiPad || isiPhone || isiPod || isiDevice || isAndroid || isBlackBerry || isWebOS || isWindowsPhone) || jQuery(window).width() > 1024) {
+    window.location.href="/shop/stores/search";
+}
+
 jQuery(function($){
     var $fail = $('.geo-fail');
     var $wait = $('.geo-wait');
