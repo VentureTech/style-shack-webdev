6dab83d25f2277ab4742dd144709796e487d8a9c
Kaitlyn Noone <knoone@vipasolutions.com>
2015-10-07 17:14:08 -0500

    Moved to be triggered after header loads

--- web/src/javascript/components/blog-filters.js
+++ web/src/javascript/components/blog-filters.js
@@ -1,21 +1,24 @@
-/**
- * Created by webdev on 8/7/15.
- */
-jQuery(function($){
-    var $mainNav = $(".main-nav");
-    var blogCats = [".spotlights", ".trends", ".lifeshack"];
+(function ($, d) {
 
+    function initHeader() {
+        var $mainNav = $(".main-nav");
+        var blogCats = [".spotlights", ".trends", ".lifeshack"];
 
-    for (var i = 0; i < blogCats.length; i++) {
-        var $con = $(blogCats[i]+".render");
-        if ($con.length) {
-            appendBlogCatsToMenu($con, blogCats[i]);
+        for (var i = 0; i < blogCats.length; i++) {
+            var $con = $(blogCats[i] + ".render");
+            if ($con.length) {
+                appendBlogCatsToMenu($con, blogCats[i]);
+            }
+        }
+
+        function appendBlogCatsToMenu($con, classname) {
+            $mainNav.find(classname).addClass("mi-parent").append($con);
         }
     }
 
-    function appendBlogCatsToMenu($con, classname) {
-        $mainNav.find(classname).addClass("mi-parent").append($con);
-    }
-});
+    $(d).on("ss:header-init", initHeader);
+
+})(jQuery, document);
+
 
 
