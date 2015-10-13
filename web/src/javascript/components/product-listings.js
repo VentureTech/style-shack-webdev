33300a8a72dd53691d8827a182e3d5f39dd433ad
Kaitlyn Noone <knoone@vipasolutions.com>
2015-10-13 17:15:50 -0500

    Changes to better handle .sm listings

--- web/src/javascript/components/product-listings.js
+++ web/src/javascript/components/product-listings.js
@@ -3,6 +3,7 @@ jQuery(function($){
     var $productsListingCon = $(".product-listing");
     var MOBILE_BREAKPOINT = 480;
     var TABLET_BREAKPOINT = 768;
+    var DESKTOP_BREAKPOINT = 1024;
     var RESIZE_THROTTLE_TIME = 200;
 
     var resizeThrottleId;
@@ -38,21 +39,36 @@ jQuery(function($){
                 var $product = $(product);
                 $product.removeClass(COL_ONE_CLASS).removeClass(COL_TWO_CLASS).removeClass(COL_THREE_CLASS).removeClass(COL_FOUR_CLASS);
 
-                /** HANDLE FOUR COLS ABOVE TABLET LANDSCAPE **/
                 if (windowWidth >= TABLET_BREAKPOINT) {
-                    if (idx % 4 == 3) {
-                        $product.addClass(COL_FOUR_CLASS);
-                    }
-                    else if (idx % 4 == 2) {
-                        $product.addClass(COL_THREE_CLASS);
-                    }
-                    else if (idx % 4 == 1) {
-                        $product.addClass(COL_TWO_CLASS);
+                    if ($(listing).hasClass('sm')) {
+                        /** HANDLE THREE COLS ABOVE TABLET PORTRAIT **/
+                        if (idx % 3 == 2) {
+                            $product.addClass(COL_THREE_CLASS);
+                        }
+                        else if (idx % 3 == 1) {
+                            $product.addClass(COL_TWO_CLASS);
+                        }
+                        else {
+                            $product.addClass(COL_ONE_CLASS);
+                        }
                     }
                     else {
-                        $product.addClass(COL_ONE_CLASS);
+                        /** HANDLE FOUR COLS ABOVE TABLET PORTRAIT **/
+                        if (idx % 4 == 3) {
+                            $product.addClass(COL_FOUR_CLASS);
+                        }
+                        else if (idx % 4 == 2) {
+                            $product.addClass(COL_THREE_CLASS);
+                        }
+                        else if (idx % 4 == 1) {
+                            $product.addClass(COL_TWO_CLASS);
+                        }
+                        else {
+                            $product.addClass(COL_ONE_CLASS);
+                        }
                     }
                 }
+
 
                 /** HANDLE THREE COLS BELOW TABLET LANDSCAPE AND ABOVE TABLET PORTRAIT **/
                 else if (windowWidth < TABLET_BREAKPOINT && windowWidth > MOBILE_BREAKPOINT) {
