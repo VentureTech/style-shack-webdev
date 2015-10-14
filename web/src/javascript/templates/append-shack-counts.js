756ee152031e1297bef328024f0364cb6033235c
Kaitlyn Noone <knoone@vipasolutions.com>
2015-10-14 17:03:55 -0500

    Changes functionality to handle the counts loading in delayed

--- web/src/javascript/templates/append-shack-counts.js
+++ web/src/javascript/templates/append-shack-counts.js
@@ -1,13 +1,38 @@
 jQuery(function($){
     var $userMenu = $(".menu.user-links");
-    var $productCount = $('.product-count');
-    var $storeCount = $('.store-count');
+    var $productCount;
+    var $storeCount;
+    var pollCount = 0;
 
-    $userMenu.each(function(idx, menu){
-        var $menu = $(menu);
-        var $myShackItem = $menu.find(".shack .menuitemlabel");
-        var $followingItem = $menu.find(".following .menuitemlabel");
-        $myShackItem.append("<span class='num'>(" + $productCount.text().trim() + ")</span>");
-        $followingItem.append("<span class='num'>(" + $storeCount.text().trim() + ")</span>");
-    });
+    /* Function to find store/product count components after they are loaded */
+    function pollForCounts() {
+        setTimeout(function(){
+            if (($(".product-count").length && $(".store-count").length) || pollCount > 101) {
+                $productCount = $('.product-count');
+                $storeCount = $('.store-count');
+                appendCountsToNav();
+            }
+            else {
+                pollForCounts();
+            }
+        },50);
+        pollCount++;
+    }
+
+    function appendCountsToNav() {
+        $userMenu.each(function (idx, menu) {
+            var $menu = $(menu);
+            var $myShackItem = $menu.find(".shack .menuitemlabel");
+            var $followingItem = $menu.find(".following .menuitemlabel");
+            if ($productCount.text().length) {
+                $myShackItem.append("<span class='num'>(" + $productCount.text().trim() + ")</span>");
+            }
+
+            if ($storeCount.text().length) {
+                $followingItem.append("<span class='num'>(" + $storeCount.text().trim() + ")</span>");
+            }
+        });
+    }
+
+    pollForCounts();
 });
