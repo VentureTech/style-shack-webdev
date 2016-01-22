944a5a8adf3fde68ebc7e62b0b62287073c2a628
capriscoza <yic_j4d@inmrkt.com>
2016-01-22 16:48:35 -0600

    GetAmbassador script for integration.

---
+++ web/src/javascript/templates/ambassador.js
@@ -0,0 +1,12 @@
+/**
+ * Script from GetAmbassador for integration
+ */
+
+(function (u, n, i, v, e, r, s, a, l) {
+    u[r] = {};
+    u[r].uid = '4d72e3c2-3526-4f21-8a4a-4a6a44e0cb7e';
+    a = n.createElement(v);
+    a.src = e; a.async = s;
+    n.getElementsByTagName(i)[0].appendChild(a);
+})
+(window, document, 'head', 'script', 'https://cdn.getambassador.com/us.js', 'mbsy', true);
